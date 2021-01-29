6444be8d6f0510b7acf57ac19445655db990aa41
Carlos González <carlos.rgn@gmail.com>
2021-01-28 20:56:35 -0600

    optional PORT env

--- src/webapi/src/main.ts
+++ src/webapi/src/main.ts
@@ -4,9 +4,13 @@ import { AppModule } from 'src/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT;
+  let port = process.env.PORT;
   if (typeof port === 'undefined') {
-    throw new Error('Port not defined.');
+    if (process.env.NODE_ENV === 'development') {
+      port = '3000';
+    } else {
+      throw new Error('Port not defined.');
+    }
   }
   await app.listen(port, () => {
     Logger.log(`Listening on port ${port}`);
