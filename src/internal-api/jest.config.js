f46ab6dd90b97244a94fea011fb509f2716a1029
Carlos González <carlos.rgn@gmail.com>
2021-02-16 16:37:25 -0600

    ok

---
+++ src/internal-api/jest.config.js
@@ -0,0 +1,7 @@
+const base = require('../../jest.config');
+
+module.exports = {
+  ...base,
+  rootDir: 'src',
+  testRegex: [/.*\.spec\.ts$/],
+};
---
+++ src/internal-api/src/main.ts
@@ -0,0 +1,19 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from 'src/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  let port = process.env.PORT;
+  if (typeof port === 'undefined') {
+    if (process.env.NODE_ENV === 'development') {
+      port = '3000';
+    } else {
+      throw new Error('Port not defined.');
+    }
+  }
+  await app.listen(port, () => {
+    Logger.log(`Listening on port ${port}.`);
+  });
+}
+bootstrap();
