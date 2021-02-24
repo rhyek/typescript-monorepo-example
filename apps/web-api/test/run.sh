12e791b756a12ce0d7fdc83319d526f4f4d77f83
Carlos González <carlos.rgn@gmail.com>
2021-02-23 20:46:46 -0600

    delete pr images

--- apps/web-api/test/run.sh
+++ apps/web-api/test/run.sh
@@ -5,11 +5,12 @@ set -e
 export COMPOSE_PROJECT_NAME=web-api-e2e
 export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1
+export INTERNAL_API_PORT=4001
 
-WHICH=$([ "$CI" = "true" ] && echo 'ci' || echo 'dev')
+WHICH=$([ "$CI" == "true" ] && echo 'ci' || echo 'dev')
 docker-compose -f docker-compose.yaml -f "docker-compose.$WHICH.yaml" up --build --detach
-sh -s -- localhost:4001 -t 30 -- echo "Internal API available at 4001." < <(curl -s https://raw.githubusercontent.com/eficode/wait-for/v1.2.0/wait-for)
+sh -s -- localhost:4001 -t 30 -- echo "Internal API available at $INTERNAL_API_PORT." < <(curl -s https://raw.githubusercontent.com/eficode/wait-for/v1.2.0/wait-for)
 
 ttsc -b ../tsconfig.test.json
 
-INTERNAL_API_PORT=4001 jest --runInBand
+jest --runInBand
