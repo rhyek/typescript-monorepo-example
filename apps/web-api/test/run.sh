67549613cee48539eb01425c9a4a3f8b4c40d903
Carlos González <carlos.rgn@gmail.com>
2021-02-23 20:28:31 -0600

    delete pr images

--- apps/web-api/test/run.sh
+++ apps/web-api/test/run.sh
@@ -8,7 +8,7 @@ export DOCKER_BUILDKIT=1
 
 WHICH=$([ "$CI" = "true" ] && echo 'ci' || echo 'dev')
 docker-compose -f docker-compose.yaml -f "docker-compose.$WHICH.yaml" up --build --detach
-sh -s -- localhost:4001 -t 30 -- echo "Success." < <(curl -s https://raw.githubusercontent.com/eficode/wait-for/v1.2.0/wait-for)
+sh -s -- localhost:4001 -t 30 -- echo "Internal API available at 4001." < <(curl -s https://raw.githubusercontent.com/eficode/wait-for/v1.2.0/wait-for)
 
 ttsc -b ../tsconfig.test.json
 
