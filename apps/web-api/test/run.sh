2381472a90b768bb5c024e240e31a9266d450172
Carlos González <carlos.rgn@gmail.com>
2021-02-24 09:20:04 -0600

    single internal api port definition

--- apps/web-api/test/run.sh
+++ apps/web-api/test/run.sh
@@ -9,7 +9,7 @@ export INTERNAL_API_PORT=4001
 
 WHICH=$([ "$CI" == "true" ] && echo 'ci' || echo 'dev')
 docker-compose -f docker-compose.yaml -f "docker-compose.$WHICH.yaml" up --build --detach
-sh -s -- localhost:4001 -t 30 -- echo "Internal API available at $INTERNAL_API_PORT." < <(curl -s https://raw.githubusercontent.com/eficode/wait-for/v1.2.0/wait-for)
+sh -s -- "localhost:$INTERNAL_API_PORT" -t 30 -- echo "Internal API available at $INTERNAL_API_PORT." < <(curl -s https://raw.githubusercontent.com/eficode/wait-for/v1.2.0/wait-for)
 
 ttsc -b ../tsconfig.test.json
 
