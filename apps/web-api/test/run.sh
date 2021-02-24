d2a7369c906884402dfff86e9426820e0a158fec
Carlos González <carlos.rgn@gmail.com>
2021-02-24 12:17:52 -0600

    curl internal api

--- apps/web-api/test/run.sh
+++ apps/web-api/test/run.sh
@@ -7,6 +7,7 @@ export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1
 export INTERNAL_API_PORT=4001
 
+docker -v
 WHICH=$([ "$CI" == "true" ] && echo 'ci' || echo 'dev')
 docker-compose -f docker-compose.yaml -f "docker-compose.$WHICH.yaml" up --build --detach
 sh -s -- "localhost:$INTERNAL_API_PORT" -t 30 -- echo "Internal API available at $INTERNAL_API_PORT." < <(curl -s https://raw.githubusercontent.com/eficode/wait-for/v1.2.0/wait-for)
