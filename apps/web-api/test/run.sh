31422a6a2005c49305c7b241f3afceaa055d67a3
Carlos González <carlos.rgn@gmail.com>
2021-02-23 20:19:35 -0600

    delete pr images

---
+++ apps/web-api/test/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+export COMPOSE_PROJECT_NAME=web-api-e2e
+export COMPOSE_DOCKER_CLI_BUILD=1
+export DOCKER_BUILDKIT=1
+
+WHICH=$([ "$CI" = "true" ] && echo 'ci' || echo 'dev')
+docker-compose -f docker-compose.yaml -f "docker-compose.$WHICH.yaml" up --build --detach
+sh -s -- localhost:4001 -t 30 -- echo "Success." < <(curl -s https://raw.githubusercontent.com/eficode/wait-for/v1.2.0/wait-for)
+
+ttsc -b ../tsconfig.test.json
+
+INTERNAL_API_PORT=4001 jest --runInBand
