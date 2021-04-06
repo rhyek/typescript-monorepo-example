3f6f4c57b085384e4568eb62f451a26ed6923c08
Carlos González <carlos.rgn@gmail.com>
2021-04-05 18:56:07 -0600

    token

--- .github-src/includes/image_helpers.sh
+++ .github-src/includes/image_helpers.sh
@@ -1,30 +1,13 @@
-# https://stackoverflow.com/a/9894126/410224
-trap "exit 1" TERM
-export TOP_PID=$$
-
 get_image_id () {
   local PACKAGE_NAME=$1
   local TAG=$2
 
-  code=$(curl -s \
-    --output out.txt \
-    --write-out "%{http_code}" \
-    -H "Authorization: Bearer ${{ secrets.DOCKER_TOKEN }}" \
+  curl -s \
+    -H "Authorization: Bearer $DOCKER_TOKEN" \
     -H 'Accept: application/vnd.github.v3+json' \
-    "https://api.github.com/$PACKAGES_PATH_PREFIX/packages/container/$PACKAGE_NAME/versions"
-  )
-  if [ $code -eq 200 ]; then
-    cat out.txt | jq -rM "first(.[] | select(.metadata.container.tags | index(\"$TAG\"))).id // null"
-  else
-    if [ $code -eq 404 ]; then
-      echo 'null'
-      return 0
-    else
-      echo "http status: $code"
-      cat out.txt
-      kill -s TERM $TOP_PID
-    fi
-  fi
+    "https://api.github.com/$PACKAGES_PATH_PREFIX/packages/container/$PACKAGE_NAME/versions" \
+    | jq -rM "first(.[] | select(.metadata.container.tags | index(\"$TAG\"))).id // null" \
+    || echo null
 }
 
 delete_image () {
@@ -33,7 +16,7 @@ delete_image () {
 
   curl -i \
     -X DELETE \
-    -H "Authorization: Bearer ${{ secrets.DOCKER_TOKEN }}" \
+    -H "Authorization: Bearer $DOCKER_TOKEN" \
     -H "Accept: application/vnd.github.v3+json" \
     "https://api.github.com/$PACKAGES_PATH_PREFIX/packages/container/$PACKAGE_NAME/versions/$IMAGE_ID"
 }
